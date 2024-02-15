0d8d2801d67761e74eb62b850726dced341f6668
Jules-gatete <j.gatete@alustudent.com>
2024-02-15 22:35:04 +0000

    change

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,15 +11,16 @@ echo "Hello World!" | sudo tee /data/web_static/releases/test/index.html
 #Create symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 #Give ownership of the /data/ folder to the ubuntu user AND group
-sudo chown -hR ubuntu:ubuntu /data
+sudo chown -R ubuntu:ubuntu /data
 
 # Update the Nginx configuration
 config="location /hbnb_static/ {
     alias /data/web_static/current/;
-    index index.html";
+    index index.html;
 }"
 
-sudo sed -i "/5857-web-01;/a $config" /etc/nginx/sites-available/default
+sudo sed -i "/server_name web-01;/a $config" /etc/nginx/sites-available/default
 
 # Restart Nginx
 sudo service nginx restart
+
